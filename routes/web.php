b16819d23c0fc9429f5d173d45ad227f7b61d725
root <dhimasanb@gmail.com>
2018-03-23 08:36:07 +0000

    Implementasi MVC Pattern

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::get('/', 'PostController@index');
+
 
 Route::get('/profile', function () {
     return view('profile');
