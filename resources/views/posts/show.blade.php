6cb0fdb9e0bfd03ff3f4ecefac753164868a89a7
rfkchrl <rfkchrl.03@gmail.com>
2018-05-25 14:35:06 +0700

    Buat method show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,11 +9,11 @@
       <form method="POST" action="{{ route('posts.store')}}">
         <div class="form-group">
           <label for="usr">Title</label>
-          <input type="text" class="form-control" id="title" name="title" required>
+          <input type="text" class="form-control" id="title" name="title" value="{{$post->title}}" required>
         </div>
         <div class="form-group">
-          <label for="pwd">Body</label>
-          <textarea class="form-control" rows="5" id="body" name="body" required></textarea>
+          <label for="pwd">Content</label>
+          <textarea class="form-control" rows="5" id="content" name="content"  required>{{$post->content}}</textarea>
         </div>
         <button class="btn btn-success btn-lg btn-block" type="submit">Create Post</button>
       </form>
