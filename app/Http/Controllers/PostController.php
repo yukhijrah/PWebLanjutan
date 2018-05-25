8c6df8f5e3556bda3dd30a930a40f116ce2e4e06
rfkchrl <rfkchrl.03@gmail.com>
2018-05-25 16:07:53 +0700

    buat method destroy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -121,5 +121,9 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+        $post = Post::find($id);
+        $post -> delete();
+
+        return redirect()->route('posts.index');
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -35,7 +35,17 @@
                             <td>{{ $post->title }}</td>
                             <td>{{ substr(strip_tags($post->content), 0, 50) }}{{ strlen(strip_tags($post->content)) > 50 ? "..." : "" }}</td>
                             <td>{{ date('M j, Y', strtotime($post->created_at)) }}</td>
-                            <td><a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a> <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a></td>
+                            <td>
+                                <a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a> 
+                                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a>
+                               
+                                
+                                <form method="POST" action="{{route('posts.destroy', $post->id)}}">
+                                        {{ method_field('DELETE') }}
+                                        {{ csrf_field() }}
+                                <button type="submit" class="btn btn-default btn-sm">Delete</button>
+                                </form>
+                            </td>
                         </tr>
 
                     @endforeach
